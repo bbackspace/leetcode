0b0b561c60e3709be44371b7f40fca9e0a30b758
Joel Manish Pinto <joelm.pinto@gmail.com>
2024-03-13 12:47:51 +0530

    1171. Remove Zero Sum Consecutive Nodes from Linked List

---
+++ problems/remove-zero-sum-consecutive-nodes-from-linked-list/src/Solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        var list = new ArrayList<ListNode>();
+        list.add(new ListNode(0, head));
+        for (ListNode p = head; p != null; p = p.next) {
+            list.add(p);
+        }
+        var prefixSum = new ArrayList<Integer>();
+        int n = list.size() - 1;
+        prefixSum.add(list.get(1).val);
+        for (int i = 1; i < n; i++) {
+            prefixSum.add(prefixSum.get(i - 1) + list.get(i + 1).val);
+        }
+        int i = 0;
+        while (i < n) {
+            boolean cut = false;
+            for (int j = i; j < n; j++) {
+                if (prefixSum.get(j) == (i == 0 ? 0 : prefixSum.get(i - 1))) {
+                    list.get(i).next = list.get(j + 1).next;
+                    i = j + 1;
+                    cut = true;
+                    break;
+                }
+            }
+            if (!cut) {
+                i++;
+            }
+        }
+        return list.get(0).next;
+    }
+}
